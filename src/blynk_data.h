a790fab281985f08bcffbd4070f572365cdd725b
almaz8811 <almaz8811@gmail.com>
2019-10-24 01:32:02 +0300

    Основа программы

---
+++ src/blynk_data.h
@@ -0,0 +1,50 @@
+BLYNK_WRITE(V0) // Пресеты
+{
+    if (param.asInt() == 1)
+    { // Курица
+        time_sushki = 24;
+        rejim = 2;
+        temp_sushki = 10;
+    }
+    else if (param.asInt() == 2)
+    { // Курица +
+        time_sushki = 24;
+        rejim = 3;
+        temp_sushki = 25;
+    }
+    else if (param.asInt() == 3)
+    { // Говядина
+        time_sushki = 48;
+        rejim = 2;
+        temp_sushki = 10;
+    }
+    else if (param.asInt() == 4)
+    { // Говядина +
+        time_sushki = 48;
+        rejim = 3;
+        temp_sushki = 30;
+    }
+    Blynk.virtualWrite(1, time_sushki);
+    Blynk.virtualWrite(2, rejim);
+    Blynk.virtualWrite(3, temp_sushki);
+}
+
+BLYNK_WRITE(V1) // Время сушки
+{
+    time_sushki = param.asInt();
+}
+
+BLYNK_WRITE(V2) // Режим сушки
+{
+    rejim = param.asInt();
+}
+
+BLYNK_WRITE(V3) // Температура сушки
+{
+    temp_sushki = param.asInt();
+}
+
+BLYNK_WRITE(V5) // реле
+{
+    digitalWrite(4, param.asInt());
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include "setup.h"
+#include "blynk_init.h"
+#include "blynk_data.h"
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println();
+  pinMode(4, OUTPUT);
+  blynk_init();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  Blynk.run();
+  httpServer.handleClient();
+  MDNS.update();
 }
