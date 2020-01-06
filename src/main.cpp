ca3567a48b9996f6365f2bd8014c196f54849baf
AlMaz <almaz8811@gmail.com>
2020-01-06 21:40:36 +0300

    1

--- src/main.cpp
+++ src/main.cpp
@@ -4,81 +4,81 @@
 #include "blynk_init.h"
 #include "blynk_data.h"
 
-void readData()
-{
-  EEPROM.get(0, preset);
-  EEPROM.get(1, time_sushki);
-  EEPROM.get(2, rejim);
-  EEPROM.get(3, temp_sushki);
-  Blynk.virtualWrite(V0, preset);
-  Blynk.virtualWrite(V1, time_sushki);
-  Blynk.virtualWrite(V2, rejim);
-  Blynk.virtualWrite(V3, temp_sushki);
-}
+// void readData()
+// {
+//   EEPROM.get(0, preset);
+//   EEPROM.get(1, time_sushki);
+//   EEPROM.get(2, rejim);
+//   EEPROM.get(3, temp_sushki);
+//   Blynk.virtualWrite(V0, preset);
+//   Blynk.virtualWrite(V1, time_sushki);
+//   Blynk.virtualWrite(V2, rejim);
+//   Blynk.virtualWrite(V3, temp_sushki);
+// }
 
 
-void start()
-{
-  saveData();
-  if (checkStart == 1)
-  {
-    switch (rejim)
-    {
-    case 1:
-      digitalWrite(14, !HIGH);
-      digitalWrite(12, !HIGH);
-      digitalWrite(13, !LOW);
-      digitalWrite(15, !LOW);
-      Blynk.virtualWrite(V5, 255);
-      Blynk.virtualWrite(V6, 255);
-      Blynk.virtualWrite(V7, 0);
-      Blynk.virtualWrite(V8, 0);
-      break;
-    case 2:
-      digitalWrite(14, !HIGH);
-      digitalWrite(12, !HIGH);
-      digitalWrite(13, !HIGH);
-      digitalWrite(15, !LOW);
-      Blynk.virtualWrite(V5, 255);
-      Blynk.virtualWrite(V6, 255);
-      Blynk.virtualWrite(V7, 255);
-      Blynk.virtualWrite(V8, 0);
-      break;
-    case 3:
-      digitalWrite(14, !HIGH);
-      digitalWrite(12, !HIGH);
-      digitalWrite(13, !HIGH);
-      digitalWrite(15, !HIGH);
-      Blynk.virtualWrite(V5, 255);
-      Blynk.virtualWrite(V6, 255);
-      Blynk.virtualWrite(V7, 255);
-      Blynk.virtualWrite(V8, 255);
-      break;
-    }
-  }
-  else
-  {
-    digitalWrite(14, !LOW);
-    digitalWrite(13, !LOW);
-    digitalWrite(12, !LOW);
-    digitalWrite(15, !LOW);
-    Blynk.virtualWrite(V5, 0);
-    Blynk.virtualWrite(V6, 0);
-    Blynk.virtualWrite(V7, 0);
-    Blynk.virtualWrite(V8, 0);
-  }
-}
+// void start()
+// {
+//   saveData();
+//   if (checkStart == 1)
+//   {
+//     switch (rejim)
+//     {
+//     case 1:
+//       digitalWrite(14, !HIGH);
+//       digitalWrite(12, !HIGH);
+//       digitalWrite(13, !LOW);
+//       digitalWrite(15, !LOW);
+//       Blynk.virtualWrite(V5, 255);
+//       Blynk.virtualWrite(V6, 255);
+//       Blynk.virtualWrite(V7, 0);
+//       Blynk.virtualWrite(V8, 0);
+//       break;
+//     case 2:
+//       digitalWrite(14, !HIGH);
+//       digitalWrite(12, !HIGH);
+//       digitalWrite(13, !HIGH);
+//       digitalWrite(15, !LOW);
+//       Blynk.virtualWrite(V5, 255);
+//       Blynk.virtualWrite(V6, 255);
+//       Blynk.virtualWrite(V7, 255);
+//       Blynk.virtualWrite(V8, 0);
+//       break;
+//     case 3:
+//       digitalWrite(14, !HIGH);
+//       digitalWrite(12, !HIGH);
+//       digitalWrite(13, !HIGH);
+//       digitalWrite(15, !HIGH);
+//       Blynk.virtualWrite(V5, 255);
+//       Blynk.virtualWrite(V6, 255);
+//       Blynk.virtualWrite(V7, 255);
+//       Blynk.virtualWrite(V8, 255);
+//       break;
+//     }
+//   }
+//   else
+//   {
+//     digitalWrite(14, !LOW);
+//     digitalWrite(13, !LOW);
+//     digitalWrite(12, !LOW);
+//     digitalWrite(15, !LOW);
+//     Blynk.virtualWrite(V5, 0);
+//     Blynk.virtualWrite(V6, 0);
+//     Blynk.virtualWrite(V7, 0);
+//     Blynk.virtualWrite(V8, 0);
+//   }
+// }
 
-void readSens() {
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  Blynk.virtualWrite(V9, h);
-  Blynk.virtualWrite(V10, t);
-}
+// void readSens() {
+//   h = dht.readHumidity();
+//   t = dht.readTemperature();
+//   if (isnan(h) || isnan(t)) {
+//     Serial.println("Failed to read from DHT sensor!");
+//     return;
+//   }
+//   Blynk.virtualWrite(V9, h);
+//   Blynk.virtualWrite(V10, t);
+// }
 
 void setup()
 {
@@ -91,7 +91,7 @@ void setup()
   pinMode(15, OUTPUT); // Подогрев
   blynk_init();
   dht.begin();
-  timer.setInterval(1000L, readSens);
+  //timer.setInterval(1000L, readSens);
   //Blynk.syncAll();
 }
 
@@ -104,15 +104,7 @@ void loop()
   MDNS.update();
   if (millis() - myTimer >= 500)
   {
-    start();
+    //start();
     myTimer = millis();
   }
-}
-
-void saveData()
-{
-  EEPROM.put(0, preset);
-  EEPROM.put(1, time_sushki);
-  EEPROM.put(2, rejim);
-  EEPROM.put(3, temp_sushki);
 }
